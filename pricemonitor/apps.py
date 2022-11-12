6b234ce2eb91e8c6193fe3f9cbb81938615fb984
amatacz <aleksandra.matacz93@gmail.com>
2022-11-12 09:03:46 +0100

    templates updated

--- pricemonitor/apps.py
+++ pricemonitor/apps.py
@@ -5,11 +5,6 @@ class PricemonitorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pricemonitor'
 
-<<<<<<< HEAD
     
     def ready(self):
         import pricemonitor.signals
-=======
-    def ready(self):
-        import pricemonitor.signals
->>>>>>> 83c5cfa6f4e0dd156aa2811706d5d49ab811c8c4
--- pricemonitor/permissions.py
+++ pricemonitor/permissions.py
@@ -1,19 +1,11 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 
 
-<<<<<<< HEAD
 class ModeratorPermisionMixin(PermissionRequiredMixin):
-=======
-class UserPermisionMixin(PermissionRequiredMixin):
->>>>>>> 83c5cfa6f4e0dd156aa2811706d5d49ab811c8c4
 
     def has_permission(self):
         return self.request.user.profile.role == 'moderator'
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 83c5cfa6f4e0dd156aa2811706d5d49ab811c8c4
 class UserRequestPermisionMixin(PermissionRequiredMixin):
 
     def has_permission(self):
