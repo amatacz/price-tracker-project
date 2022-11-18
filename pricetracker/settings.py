1ed82f8a3bd9528834191b97b7a6bed54a8aa04c
amatacz <aleksandra.matacz93@gmail.com>
2022-11-18 20:21:39 +0100

    settings corrected

--- pricetracker/settings.py
+++ pricetracker/settings.py
@@ -131,12 +131,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-from .sitesettings import *
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+from .sitesettings import *
 
 
